368ad8849cb8a2f01f56b666b8b217e96e0cd6ba
techmahendran <79784374+techmahendran@users.noreply.github.com>
2023-01-23 23:00:58 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+const quoteText = document.querySelector('.quote');
+const quote_btn = document.querySelector('.quote_btn');
+const author_name = document.querySelector('.author');
+// btns
+const speak = document.querySelector('.speak');
+const copy_text = document.querySelector('.copy_text');
+
+quote_btn.addEventListener('click', randamQuote)
+speak.addEventListener('click', speakText)
+copy_text.addEventListener('click', copyText)
+
+// randamQuote run on api
+async function randamQuote() {
+   quote_btn.innerHTML = 'Loading...'
+   quote_btn.classList.add('loading')
+   const res = await fetch('https://api.quotable.io/random');
+   const result_1 = await res.json();
+   quoteText.innerHTML = result_1.content;
+   author_name.innerHTML = result_1.author;
+   quote_btn.innerHTML = 'New Quote'
+   quote_btn.classList.remove('loading')
+}
+
+function speakText() {
+    // speak the quote text
+    let sentence = new SpeechSynthesisUtterance(`${quoteText.innerHTML} by ${author_name.innerHTML}`)
+    speechSynthesis.speak(sentence)
+}
+
+function copyText() {
+    // copy the quote text
+    navigator.clipboard.writeText(quoteText.innerHTML)
+}
